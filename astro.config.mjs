53e5cad872b420cf19ee1dd167323eb0f9de2656
d0ty <hp.raszpi@gmail.com>
2025-06-05 21:41:05 +0200

    netlify adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import {astroImageTools} from "astro-imagetools";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    integrations: [astroImageTools]
+  output: 'server',
+  integrations: [astroImageTools],
+  adapter: netlify()
 });
